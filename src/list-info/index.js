8b503b75bec0164e2a7841ed3fc7794fa533ba01
Sneha <snehapatildeepali@gmail.com>
2024-01-15 07:05:28 +0530

    Add functionality.

--- src/list-info/index.js
+++ src/list-info/index.js
@@ -3,6 +3,7 @@ import './style.scss';
 import Edit from './edit';
 import save from './save';
 import { __ } from '@wordpress/i18n';
+
 import '../list-datastore/index';
 
 registerBlockType('blocks-todo/info', {
